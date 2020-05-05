8ec59f85fbb9874832f5bba5312a64e265e31e14
Vinicius Passos <viniciuspa87@hotmail.com>
2020-05-04 21:01:30 -0300

    Ajuste na estrutura e CSS, login e cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,17 +6,13 @@
         <div class="col-md-8">
             <div class="card  special-card">
                 <img class="img-logo rounded mx-auto d-block" src="{{ asset('img/logo.svg') }} " alt="Responsive image">
-
+                <h4 class="rounded mx-auto d-block text-white">Faça seu Cadastro</h4>
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror input-icon" placeholder="&#xF2c0; Nome" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                            <div class="col-md-6 offset-md-3">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Nome" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -26,10 +22,8 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror input-icon" placeholder="&#xF0e0; E-mail" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="E-mail" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,11 +34,8 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input-icon" placeholder="&#xF023; Senha" name="password" required autocomplete="new-password">
-
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Senha" name="password" required autocomplete="new-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -54,26 +45,26 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control input-icon" placeholder="&#xF023; Confirme sua Senha" name="password_confirmation" required autocomplete="new-password">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password-confirm" type="password" class="form-control" placeholder="Confirme sua Senha" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3">
                                 <button type="submit" class="btn btn-warning btn-block">
-                                    {{ __('Register') }}
+                                    Cadastro
                                 </button>
                             </div>
                         </div>
-                        <div class="col-md-6 offset-md-5 item-criar ">
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link text-white" href="{{ route('home') }}">
-                                    {{ __('Voltar para login') }}
-                                </a>
-                            @endif
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3 d-flex">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link text-white width-100" href="{{ route('home') }}">
+                                        {{ __('Voltar para login') }}
+                                    </a>
+                                @endif
+                            </div>
                         </div>
                     </form>
                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             @guest
             <div class="card special-card">
-                
+
                 <img class="img-logo rounded mx-auto d-block" src="{{ asset('img/logo.svg') }} " alt="Responsive image">
 
                 <h4 class="rounded mx-auto d-block text-white">Faça seu login</h4>
@@ -21,13 +21,8 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right"></label>
-                            
-
-                            <div class="col-md-6">
-                                   
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror input-icon" name="email" value="{{ old('email') }}" required autocomplete="email"  placeholder="&#xF0e0; E-mail" autofocus>
-                               
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"  placeholder="E-mail" autofocus>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -37,11 +32,8 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right"></label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input-icon" name="password" placeholder="&#xF023; Senha" required autocomplete="current-password">
-
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Senha" required autocomplete="current-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -49,31 +41,36 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3">
                                 <button type="submit" class="btn btn-warning btn-block">
                                     {{ __('Entrar') }}
                                 </button>
+                            </div>
                         </div>
-                        <div class="col-md-6 offset-md-5">
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3">
                             @if (Route::has('password.request'))
-                                <a class="btn btn-link text-white" href="{{ route('password.request') }}">
+                                <a class="btn btn-link text-white width-100" href="{{ route('password.request') }}">
                                     {{ __('Esqueci minha senha') }}
                                 </a>
                             @endif
+                            </div>
                         </div>
-                        <div class="col-md-6 offset-md-5 item-criar ">
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link text-white" href="{{ route('register') }}">
-                                    {{ __('Criar Conta') }}
-                                </a>
-                            @endif
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3 d-flex">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link text-white width-100" href="{{ route('register') }}">
+                                        {{ __('Criar Conta') }}
+                                    </a>
+                                @endif
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
             @endguest
-            
+
         </div>
     </div>
 </div>
